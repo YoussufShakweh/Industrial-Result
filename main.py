cd7978e6a52b79cf026ca2ea9bc2e1785adc3fc1
Youssuf Shakweh <110261583+YoussufShakweh@users.noreply.github.com>
2023-07-17 19:54:03 +0300

    main.py

--- main.py
+++ main.py
@@ -1,36 +1,45 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
-import selenium.common.exceptions as sel_ex
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import WebDriverException
+
+# Enter The Student Number
+std_num = input("Enter Your Number: ")
+
+url = "http://moed.gov.sy/12th/index.php"
+driver = webdriver.Chrome()
+
+while True:
+  try: 
+    driver.get(url)
+    break
+  except WebDriverException:
+    driver.refresh()
+
+# Select Branch
+branch = driver.find_element(by=By.ID, value="branch")
+select_branch = Select(branch)
+select_branch.select_by_value("6") # To Chose Industrial select 6
+
+# Select Sub - Branch
+pro = driver.find_element(by=By.ID, value="sub-branch")
+select_sub_branch = Select(pro)
+# Auto Select Computer Technology
+select_sub_branch.select_by_value("p06")
+
+# Create City List
+city = driver.find_element(by=By.ID, value="city")
+select_city = Select(city)
+# Auto Select Hama
+select_city.select_by_value("7")
+
+# Create Stdnum
+stdnum = driver.find_element(by=By.ID, value="stdnum")
+stdnum.send_keys(std_num)
+
+# Create Submit Button
+submit = driver.find_element(by=By.ID, value="submit")
+submit.click()
 
 
-try:
-  # Enter The Student ID
-  std_id = input("Enter Your Number: ")
-  driver = webdriver.Chrome()
-  url = f"https://moed.gov.sy/industrial/result.php"
-  driver.get(url)
-
-  # Create City List
-  city = driver.find_element(by=By.ID, value="city")
-  select_city = Select(city)
-  # Auto Select Hama
-  select_city.select_by_value("7")
-
-  # Create Profession List
-  pro = driver.find_element(by=By.ID, value="proname")
-  select_pro = Select(pro)
-  # Auto Select IT
-  select_pro.select_by_value("p06")
-
-  # Create Stdnum
-  stdnum = driver.find_element(by=By.ID, value="stdnum")
-  stdnum.send_keys(std_id)
-
-  # Create Submit Button
-  submit = driver.find_element(by=By.ID, value="Submit")
-  submit.click()
-except:
-  print(sel_ex)
-
-input("Press any key to close ...")
+input("Press to close ...")
